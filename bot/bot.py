2bfd9c40ca6c211690d223ccfd2919b4234d39dc
Philipp Bekher <blackrose00961@gmail.com>
2022-06-01 04:41:43 +0700

    asasa

--- bot/bot.py
+++ bot/bot.py
@@ -73,7 +73,7 @@ async def after_text(message):
 
     current_exercise_right_answer = db_object.execute(f"SELECT right_answer FROM questions WHERE question_id = {result[0]}")
     right_answer_object = db_object.fetchone()
-    if message.text == right_answer_object[0] and result[0] <= len(question_records):
+    if ((message.text == right_answer_object[0]) and (result[0] <= len(question_records))):
           current_right_answers_number = result[1] + 1
           db_object.execute(f"UPDATE users SET right_answers_number = %s WHERE id = {id}",
           (current_right_answers_number,))
