2a62137a791560f12231738a8af68b45ab53a28d
Philipp Bekher <blackrose00961@gmail.com>
2022-06-13 17:18:33 +0700

    asasa

--- bot/bot.py
+++ bot/bot.py
@@ -30,7 +30,7 @@ async def start(message: types.Message):
 
     id = message.from_user.id
     username = message.from_user.username
-    fullname = message.from_user.first_name + ' ' + message.from_user.last_name
+    fullname = 'hz'
     chat_id = message.chat.id
 
     db_object.execute(f"SELECT id FROM users WHERE id = {id}")
